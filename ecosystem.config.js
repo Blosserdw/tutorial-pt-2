c34776b8e951547f545b12b2683888de11f567bf
Blosser <blosserdw@gmail.com>
2018-04-28 18:42:46 -0400

    added ecosystem config file for pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'tutorial-pt-2',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-18-221-97-7.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/tutorial-ssh-key-pair.pem',
+      ref: 'origin/master',
+      repo: 'https://github.com/Blosserdw/tutorial-pt-2.git',
+      path: '/home/ubuntu/server/',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
